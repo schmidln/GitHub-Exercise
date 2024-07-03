6c07d8fc0d4e1ac5d4fc6621c0d0f6e75522d15b
schmidln <schmidln@bc.edu>
2024-07-03 08:14:30 +0200

    Initial Commit

---
+++ GitHub Exercise/GitHub_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExerciseApp.swift
+//  GitHub Exercise
+//
+//  Created by Lucas Schmidt on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
