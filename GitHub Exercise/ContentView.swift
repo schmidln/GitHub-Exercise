962d9acd4a205dec1c5c98b528edf230c0d3cdfa
schmidln <schmidln@bc.edu>
2024-07-04 17:56:34 +0200

    Version 2.0 - Green

--- GitHub Exercise/ContentView.swift
+++ GitHub Exercise/ContentView.swift
@@ -10,12 +10,15 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: "2.circle")
+                .resizable()
+                .scaledToFit()
+            Text("Version 2.0")
+                .font(.largeTitle)
+                .fontWeight(.black)
         }
         .padding()
+        .foregroundStyle(.green)
     }
 }
 
